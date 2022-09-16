adefe262515d1c60e64a1f05db194d8377598077
Hasan Kılıcı <105741983+Hasan-Kilici@users.noreply.github.com>
2022-09-16 14:40:59 +0300

    Create rollup.config.js

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+// this file will not afect the sandbox but will
+// afect the deployment and dowload
+
+import svelte from "rollup-plugin-svelte";
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import { terser } from "rollup-plugin-terser";
+
+const production = !process.env.ROLLUP_WATCH;
+
+export default {
+  input: "index.js",
+  output: {
+    sourcemap: true,
+    format: "iife",
+    name: "app",
+    file: "public/bundle.js"
+  },
+  plugins: [
+    svelte({
+      dev: !production,
+      css: css => {
+        css.write("public/bundle.css");
+      }
+    }),
+    resolve(),
+    commonjs(),
+    production && terser()
+  ]
+};
